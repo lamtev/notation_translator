c6da000d0d81d53fee97e74f27835f9b713915c8
Anton Lamtev <antonlamtev@gmail.com>
2016-09-17 23:49:28 +0300

    improve accuracy

--- cli/src/main/java/com/lamtev/notation_translator/cli/Helper.java
+++ cli/src/main/java/com/lamtev/notation_translator/cli/Helper.java
@@ -20,7 +20,7 @@ class Helper {
     }
 
     static void printVersion() {
-        System.out.println("1.2");
+        System.out.println("1.3");
     }
 
     static void printInvalidArguments() {
